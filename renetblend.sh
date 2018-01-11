ef317e433667af886beda2ba1240de99af58d9cc
AgeOf <tylerecouture@gmail.com>
2018-01-10 22:16:38 -0800

    BS4 --> bootstrap3

--- renetblend.sh
+++ renetblend.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-BLENDERLOC=/home/90158/bin/blender-2.79-linux-glibc219-x86_64/2.79
-DEVNETRENDERLOC=/home/90158/Developer/Blender-Network-Render-Additions/netrender
+BLVERS=2.79
+BLENDERLOC=/home/$USER/bin/blender-$BLVERS-linux-glibc219-x86_64/
+
+DEVNETRENDERLOC=/home/$USER/Developer/Blender-Network-Render-Additions/netrender
 
 # copy latest netrender code to Blender addons
-cp -rf $DEVNETRENDERLOC $BLENDERLOC/scripts/addons/
+cp -rf $DEVNETRENDERLOC $BLENDERLOC/$BLVERS/scripts/addons/
 
 # stop blender master and slave and others...stop all blenders
 pkill -f blender
+
+$BLENDERLOC/blender -b master.blend --addons netrender -a -noaudio -nojoystick --enable-autoexec
 
 
